c8c3347c97d6bf7ed02e8c17cc5eacbca5585a7d
yghazi <yumnaghazi@gmail.com>
2017-09-23 02:35:40 +0500

    Fix #7: modify user sql

--- .notaryconf/ntry-user.sql
+++ .notaryconf/ntry-user.sql
@@ -9,6 +9,29 @@ CREATE TABLE IF NOT EXISTS `user` (
   `address` varchar(250) DEFAULT "",
   `account_verified` bool DEFAULT false,
   `eth_verification` varchar(64) DEFAULT "",
+  `country` varchar(30) DEFAULT "",
+  `city` varchar(30) DEFAULT "",
+  `state` varchar(20) DEFAULT "",
+  `avatar` varchar(35) DEFAULT "", 
+  `reg_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `uid` varchar(32) NOT NULL,
+  `eth_address` varchar(41) DEFAULT "",
+  `first_name` varchar(50) DEFAULT "",
+  `last_name` varchar(50) DEFAULT "",
+  `email_address` varchar(100) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `telephone_number` varchar(100) DEFAULT "",
+  `address` varchar(250) DEFAULT "",
+  `account_verified` bool DEFAULT false,
+  `eth_verification` varchar(64) DEFAULT "",
+  `country` varchar(30) DEFAULT "",
+  `city` varchar(30) DEFAULT "",
+  `state` varchar(20) DEFAULT "",
+  `avatar` varchar(35) DEFAULT "", 
   `reg_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`uid`)
 ) DEFAULT CHARSET=utf8;
