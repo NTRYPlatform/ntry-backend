a67d03042bcc5110b5b3def7e15322bae8712fc5
yghazi <yumnaghazi@gmail.com>
2017-10-03 01:41:39 +0500

    get avatar not authenticated

--- routers.go
+++ routers.go
@@ -22,7 +22,8 @@ func (n *Notary) muxServer() (router *mux.Router) {
 	//TODO: specify .Headers()
 	router.Handle("/user/update", Adapt(handler, UpdateUserInfo(handler), ValidateTokenMiddleware(handler, n.conf), Logging(handler))).Methods("POST")
 	router.Handle("/user/upload-avatar", Adapt(handler, UploadAvatar(handler, n.conf), ValidateTokenMiddleware(handler, n.conf), Logging(handler))).Methods("POST")
-	router.Handle("/user/get-avatar", Adapt(handler, DownloadAvatar(handler, n.conf), ValidateTokenMiddleware(handler, n.conf), Logging(handler))).
+	//Not authenticated - for client
+	router.Handle("/user/get-avatar", Adapt(handler, DownloadAvatar(handler, n.conf), Logging(handler))).
 		Queries("u", "{u}").Methods("GET")
 	router.Handle("/user/search", Adapt(handler, SearchUsers(handler), ValidateTokenMiddleware(handler, n.conf), Logging(handler))).
 		Queries("q", "{q}").Methods("GET")
